c377e8d5be6ba2b255c99b211b03c54e7f0fe446
Hom Raj Bhandari <homrajbhandari96@gmail.com>
2026-02-18 19:41:24 +0545

    updated app.jsx, app.css , Items.jsx and SingleItem.jsx for delete item

--- grocery-bud/src/components/Items.jsx
+++ grocery-bud/src/components/Items.jsx
@@ -1,11 +1,18 @@
 import SingleItem from "./SingleItem";
 import "./Items.css";
 
-const Items = ({ items, editCompleted}) => {
+const Items = ({ items, editCompleted, removeItem }) => {
   return (
     <div className="items">
       {items.map((item) => {
-        return <SingleItem key={item.id} item={item} editCompleted={editCompleted}/>;
+         return (
+          <SingleItem
+            key={item.id}
+            item={item}
+            editCompleted={editCompleted}
+            removeItem={removeItem}
+          />
+        );
       })}
     </div>
   );
